73562fdc78e291a6498da416d0b98cee71293284
kvohra95 <kabeer@vohra.eu>
2015-09-09 16:44:45 +0530

    lots of bug fixes

--- action/resetpassword.php
+++ action/resetpassword.php
@@ -6,6 +6,10 @@
  * Time: 3:34 PM
  */
 
+include_once $_SERVER['DOCUMENT_ROOT'] . '/database/connectdatabase.php';
+
+if (!isset($_SESSION)) session_start();
+
 if (isset($_POST["password"]) && isset($_POST["confirmpassword"]) && isset($_POST["hash"]) && isset($_POST["email"]))
 {
     if ($_POST["password"] == $_POST["confirmpassword"])
@@ -13,20 +17,20 @@ if (isset($_POST["password"]) && isset($_POST["confirmpassword"]) && isset($_POS
         if ($db->resetPassword($_POST["password"], $_POST["hash"], $_POST["email"]))
         {
             $_SESSION["resetpasswordsuccess"] = "Password reset successfully";
-            header("Location: ../dashboard.php#createdomain");
+            header("Location: ../resetpassword.php");
             exit;
         }
         else
         {
             $_SESSION["resetpassworderror"] = "Password not reset due to an unknown error, please try again";
-            header("Location: ../dashboard.php#createdomain");
+            header("Location: ../resetpassword.php");
             exit;
         }
     }
     else
     {
         $_SESSION["resetpassworderror"] = "Passwords do not match, please try again";
-        header("Location: ../dashboard.php#createdomain");
+        header("Location: ../resetpassword.php");
         exit;
     }
 }
