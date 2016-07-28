87844456e7b830075c4fdaf39591d958f50683f7
Kabeer Vohra <kabeer@vohra.eu>
2016-07-28 21:58:19 +0100

    added basic structure to webhook, needs to be linked to database

--- processing/webhook.php
+++ processing/webhook.php
@@ -9,6 +9,26 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/processing/config.php';
 $input = @file_get_contents("php://input");
 $event_json = json_decode($input);
 
-// Do something with $event_json
+$event_id = $event_json->id;
+$event = \Stripe\Event::retrieve($event_id);
+
+if ($event->type == 'invoice.payment_succeeded') {
+    email_invoice_receipt($event->data->object);
+}
 
 http_response_code(200);
+
+function email_invoice_receipt($invoice) {
+    $customerID = $invoice->customer;
+    $charge = \Stripe\Charge::retrieve($invoice->charge);
+    $amount = $charge->amount;
+    update_subscription_db($customerID, $amount);
+}
+
+function update_subscription_db($customerID, $amount) {
+    if ($amount == 300) {
+
+    } elseif ($amount == 3000) {
+
+    }
+}
