18551653f3fb44a198cadff043cbce62c8f8a04b
Kabeer Vohra <kabeer@vohra.eu>
2022-12-12 16:27:11 +0100

    finished backend

--- action/action_changeemail.php
+++ action/action_changeemail.php
@@ -14,27 +14,58 @@ if (isset($_POST["currentemail"]) && isset($_POST["newemail"]))
 {
     $_POST["currentemail"] = strtolower($_POST["currentemail"]);
     $_POST["newemail"] = strtolower($_POST["newemail"]);
+
+    if ($_POST["currentemail"] == $_POST["newemail"])
+    {
+        $_SESSION["changeemailerror"] = "Both email addresses are the same. Please change to a different email address";
+        $_SESSION["changeemailerrorcurrent"] = $_POST["currentemail"];
+        $_SESSION["changeemailerrornew"] = $_POST["newemail"];
+        header("Location: ../dashboard.php#changeemail");
+        exit;
+    }
+
     if ($db->isCurrentEmail($_SESSION["token"], $_POST["currentemail"]))
     {
-        if (preg_match('/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/', $_POST["newemail"]) == 1)
+        if ($db->isNewEmail($_POST["newemail"]))
         {
-            $db->changeEmail($_SESSION["token"], $_POST["currentemail"], $_POST["newemail"]);
-            $_SESSION["changeemailsuccess"] = "Email address has been successfully changed. Please click on the verification links sent to both email addresses to verify your new email address.";
-            header("Location: ../dashboard.php#changeemail");
-            exit;
+            if (preg_match('/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/', $_POST["newemail"]) == 1)
+            {
+                if ($db->changeEmail($_SESSION["token"], $_POST["currentemail"], $_POST["newemail"]))
+                {
+                    $_SESSION["changeemailsuccess"] = "Email address has been successfully changed. Please click on the verification links sent to both email addresses to verify your new email address";
+                }
+                else
+                {
+                    $_SESSION["changeemailerror"] = "Unknown error, please try again or contact us directly";
+                    $_SESSION["changeemailerrorcurrent"] = $_POST["currentemail"];
+                    $_SESSION["changeemailerrornew"] = $_POST["newemail"];
+                }
+
+                header("Location: ../dashboard.php#changeemail");
+                exit;
+            }
+            else
+            {
+                $_SESSION["changeemailerror"] = "New email address is invalid. Please enter a valid email address.";
+                $_SESSION["changeemailerrorcurrent"] = $_POST["currentemail"];
+                $_SESSION["changeemailerrornew"] = $_POST["newemail"];
+                header("Location: ../dashboard.php#changeemail");
+                exit;
+            }
         }
         else
         {
-            $_SESSION["changeemailerror"] = "New email address is invalid. Please re-enter it.";
+            $_SESSION["changeemailerror"] = "New email address is already used. Please sign in with that account or change to a different email address";
             $_SESSION["changeemailerrorcurrent"] = $_POST["currentemail"];
             $_SESSION["changeemailerrornew"] = $_POST["newemail"];
             header("Location: ../dashboard.php#changeemail");
             exit;
         }
+
     }
     else
     {
-        $_SESSION["changeemailerror"] = "Current email address is incorrect. Please check it and try again.";
+        $_SESSION["changeemailerror"] = "Current email address is incorrect. Please check it and try again";
         $_SESSION["changeemailerrorcurrent"] = $_POST["currentemail"];
         $_SESSION["changeemailerrornew"] = $_POST["newemail"];
         header("Location: ../dashboard.php#changeemail");
---
+++ action/action_changepassword.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kabeer
+ * Date: 01/08/2015
+ * Time: 02:54
+ */
+
+include_once '../connectdatabase.php';
+
+if (!isset($_SESSION)) session_start();
+
+if(isset($_POST["oldpassword"]) && isset($_POST["newpassword"]) && isset($_POST["confirmpassword"]))
+{
+    if ($_POST["newpassword"] == $_POST["confirmpassword"])
+    {
+        if (preg_match('/^.*(?=.{7,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/', $_POST["newpassword"]) != 1)
+        {
+            $_SESSION["changepassworderror"] = "Password must be at least 7 characters long and contain an uppercase digit, a lowercase digit and a number";
+        }
+        else
+        {
+            switch ($db->changePassword($_POST["oldpassword"], $_POST["newpassword"], $_SESSION["token"]))
+            {
+                case 1:
+                    //success
+                    $_SESSION["changepasswordsuccess"] = "Password has been successfully changed";
+                    break;
+                case 2:
+                    $_SESSION["changepassworderror"] = "The old password is incorrect. Please try again";
+                    //old pass is wrong
+                    break;
+                case 3:
+                    $_SESSION["changepassworderror"] = "Unknown error, please try again or contact us directly";
+                    //unknown error
+                    break;
+            }
+        }
+    }
+    else
+    {
+        $_SESSION["changepassworderror"] = "Passwords do not match, please try again";
+    }
+
+
+header("Location: ../dashboard.php#changepassword");
+exit;
+
+}
