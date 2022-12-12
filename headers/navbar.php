9884fac09f8e628510c3c2027a1058e7bfc8dd7d
Kabeer Vohra <kabeer@vohra.eu>
2022-12-12 16:27:12 +0100

    updated references

--- headers/navbar.php
+++ headers/navbar.php
@@ -1,8 +1,8 @@
-<link href="../css/navbar.css" rel="stylesheet">
+<link href="css/navbar.css" rel="stylesheet">
 
 <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
         <div class="container topnav">
-            <a class="navbar-brand" style="padding: 10px;" href="/"><img src="../img/logo-small.png" style="height: 30px;"></a>
+            <a class="navbar-brand" style="padding: 10px;" href="/"><img src="img/logo-small.png" style="height: 30px;"></a>
             <ul class="nav navbar-nav navbar-right">
                 <?php
                     if (isset($_SESSION["token"]))
@@ -13,17 +13,17 @@
                     if($user == false) :
                     ?>
                     <li>
-                        <a href="../login.php">Login</a>
+                        <a href="login.php">Login</a>
                     </li>
                 <?php else : ?>
                     <li>
                         <a style="color: #777">Logged in as <?= $user; ?> </a>
                     </li>
                     <li>
-                        <a href="../dashboard.php">Dashboard</a>
+                        <a href="dashboard.php">Dashboard</a>
                     </li>
                     <li>
-                        <a href="../logout.php">Logout</a>
+                        <a href="logout.php">Logout</a>
                     </li>
                 <?php endif; ?>
             </ul>
